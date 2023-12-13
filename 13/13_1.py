4ea73c2cec154f26bd7df77ae7ac243451f3dcc7
Katya Potapov <kat.p28@gmail.com>
2023-12-13 00:29:36 -0500

    Refactor

--- 13/13_1.py
+++ 13/13_1.py
@@ -15,23 +15,26 @@ for l in ls:
 
 pttns.append(cur_pttn)
 
+def check_mirror(pttn):
+    is_mirror = True
+    for j in range(min(i, len(pttn)-i)):
+        # print(f"{pttn[i-j-1]=}")
+        # print(f"{pttn[i+j]=}")
+        if pttn[i-j-1] != pttn[i+j]:
+            is_mirror = False
+            break
+    if is_mirror:
+        return i
+
+
 # horizontal
 row_matches = 0
 for pttn in pttns:
     for i in range(1, len(pttn)):
         if pttn[i] == pttn[i-1]:
-            # check the rest
-            is_mirror = True
-            for j in range(min(i, len(pttn)-i)):
-                print(f"{j=}")
-                # print(f"{pttn[i-j-1]=}")
-                # print(f"{pttn[i+j]=}")
-                if pttn[i-j-1] != pttn[i+j]:
-                    is_mirror = False
-                    break
-            if is_mirror:
-                row_matches += i
-                print(f"row: {i=}")
+            maybe_match = check_mirror(pttn)
+            if maybe_match:
+                row_matches += maybe_match
 
 for i in range(len(pttns)):
     pttns[i] = np.array(pttns[i]).T.tolist()
@@ -41,16 +44,9 @@ col_matches = 0
 for pttn in pttns:
     for i in range(1, len(pttn)):
         if pttn[i] == pttn[i-1]:
-            is_mirror = True
-            for j in range(min(i, len(pttn)-i)):
-                # print(f"{pttn[i-j-1]=}")
-                # print(f"{pttn[i+j]=}")
-                if pttn[i-j-1] != pttn[i+j]:
-                    is_mirror = False
-                    break
-            if is_mirror:
-                col_matches += i
-                print(f"col: {i=}")
+            maybe_match = check_mirror(pttn)
+            if maybe_match:
+                col_matches += maybe_match
 
 print(col_matches)
 print(row_matches)
